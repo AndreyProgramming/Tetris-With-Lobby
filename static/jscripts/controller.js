a12ea57672669e86e4deb42f0e95c5dcf3275d59
danyil.korovin <danilas25up@gmail.com>
2022-03-27 20:43:28 +0300

    #17 : изменено немного игровое поле

--- static/jscripts/controller.js
+++ static/jscripts/controller.js
@@ -1,4 +1,9 @@
 document.body.onkeydown = function (e) {
+    pressedKeyDown(e.keyCode);
+};
+
+function pressedKeyDown(keyCode)
+{
     var keys = { //Клавиши
         37: 'left',
         39: 'right', //Стрелки влево и вправо
@@ -7,8 +12,15 @@ document.body.onkeydown = function (e) {
         38: 'rotate', //Вращение- стрелкой вверх
         27: 'escape' //Пауза по клавише Esc
     };
-    if (typeof (keys[e.keyCode]) != 'undefined') { //Если код клавиши допустимый,
-        keyPress(keys[e.keyCode]); //Передать его обработчику
+    if (typeof (keys[keyCode]) != 'undefined') { //Если код клавиши допустимый,
+        keyPress(keys[keyCode]); //Передать его обработчику
         render(); //и перерисовать стакан
     }
-};
+}
+
+let downButton = document.getElementById("downButton");
+
+downButton.addEventListener = function (e) {
+    console.log(123);
+    pressedKeyDown(32);
+}
